6bdd127f1eb5236f9ead2c7e0fb677986e265299
georg <georg.squared@gmail.com>
2023-04-06 12:31:48 +0300

    feat: Add a base custom embedding model
    - This is basically a copy of Karateclub Nodesketch at the moment

--- src/bipartite_embeddings/main.py
+++ src/bipartite_embeddings/main.py
@@ -1,11 +1,26 @@
-from utils import load_graph, get_train_test_samples
+from karateclub.node_embedding.neighbourhood import nodesketch
+from sklearn.linear_model import LogisticRegressionCV
+
+from bipartite_embeddings.constants import EdgeOperator
+from bipartite_embeddings.evaluator import Evaluator
+from bipartite_embeddings.idioglossia import Idioglossia
+from utils import load_graph
 
 
 def main():
     graph = load_graph()
 
-    # Get train and test samples of the graph
-    samples = get_train_test_samples(graph)
+    # TODO: Test this
+    #   - use multiple models
+    #   - adjust classifier params (?)
+    evaluator = Evaluator(
+        graph,
+        Idioglossia(dimensions=32),
+        LogisticRegressionCV(Cs=10, cv=10, scoring="roc_auc", max_iter=1000),
+        EdgeOperator.CONCAT,
+    )
+
+    print(f"ROC AUC score: {evaluator.get_roc_auc_score()}")
 
 
 if __name__ == "__main__":
